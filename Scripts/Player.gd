e7a3360397cc8d93358ed629351a9a8aa2e0a0e4
Alexander Morsey <tempthing0@gmail.com>
2022-10-01 17:13:00 -0700

    Added saved jumps, coyote time, and messed with variables.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,25 +2,51 @@ extends CharacterBody2D
 
 
 const SPEED = 80
-const JUMP_VELOCITY = -100
-const SHORT_HOP_MODIFIER = 10
+const JUMP_VELOCITY = -400
+const SHORT_HOP_MODIFIER = 5
 const APEX_JUMP_THRESHOLD = 10
 const APEX_BONUS = 1
 const MIN_FALL_SPEED = 0
 const MAX_FALL_SPEED = 0
 const GROUND_FRICTION = 0.80
+const CYOTE_TIME = 0.09
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var gravity = 100
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+# var gravity = 100
+var time_since_fall = 0
+var saved_jump = false
 
 
 func _physics_process(delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
 	
+	if is_on_floor():
+		time_since_fall = 0
+	else:
+		time_since_fall += delta
+	
+	
+	
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	elif Input.is_action_just_pressed("ui_accept") and time_since_fall < CYOTE_TIME:
+		velocity.y = JUMP_VELOCITY
+	elif Input.is_action_just_pressed("ui_accept"):
+		# Save jump
+		saved_jump = true
+	elif is_on_floor() and saved_jump:
+		velocity.y = JUMP_VELOCITY
+		saved_jump = false
+	
+	# reset saved jump
+	if Input.is_action_just_released("ui_accept"):
+		saved_jump = false
+		
+		
+		
 	
 	#  Add the gravity and chec kfor short hop
 	if Input.is_action_just_released("ui_accept") and not is_on_floor() and velocity.y < 0:
@@ -30,10 +56,10 @@ func _physics_process(delta):
 
 
 	# Increases as you get closer to the apex
-	var apex_point = inverse_lerp(0, 10, abs(velocity.y))
-
-	var apex_bonus = direction * APEX_BONUS * apex_point
-	print(apex_point)
+#	var apex_point = lerp(0, 10, inverse_lerp(0, 10, abs(velocity.y)))
+#
+#	var apex_bonus = direction * APEX_BONUS * apex_point
+#	print(apex_point)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
