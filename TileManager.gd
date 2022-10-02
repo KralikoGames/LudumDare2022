c3ee7638b2cc3b602c58c35c0f1560060b222702
unknown <williamsokol0@gmail.com>
2022-10-02 07:38:31 -0400

    branches are now growing

--- TileManager.gd
+++ TileManager.gd
@@ -3,11 +3,11 @@ extends TileMap
 var usedTiles = {}
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_cell(0,Vector2(-2,0),1,Vector2i(0,0))
+	set_cell(0,Vector2(0,0),1,Vector2i(0,0))
 	#set_cell(0,Vector2(2,0),1,Vector2i(0,0))
 	usedTiles[Vector2(0,0)] = true;
 	#get_surrounding_tiles(Vector2(0,0)
-	branchMaker(Vector2i(3,3))
+	MakeBranch(Vector2i(5,8))
 
 	pass # Replace with function body.
 
@@ -31,11 +31,24 @@ func floodFill(startTile):
 				else:
 					list.append(neighbour)
 					
-func branchMaker(endPos):
+func MakeBranch(endPos):
 	var startPos = floodFill(endPos);
 	var slope = endPos-startPos;
+	var growPos = startPos;
 	
 	print(slope);
-	#for i in range(slope.x):
-	#	set_cell(0,Vector2(-2,0),1,Vector2i(0,0))
-			
+	var posList = [];
+	
+	for i in range(slope.x):
+		posList.append('x');		
+	for i in range(slope.y):
+		posList.append('y');
+		
+	posList.shuffle()
+	for i in range(slope.x+slope.y):
+		if(posList[i] == 'x'):
+			growPos.x += 1 
+		else:
+			growPos.y -= 1;
+		await get_tree().create_timer(.2).timeout
+		set_cell(0,growPos,1,Vector2i(0,0))
